750abf5f055cab7ec1de9e5da5f296484d2fbc63
Gourab Chakraborty <19bcs118@iiitdwd.ac.in>
2021-06-06 11:28:50 +0530

    Added merger script and sample corpus 20,000 sentences

---
+++ merger_script.py
@@ -0,0 +1,13 @@
+# Put this script in the same location of the text files
+
+firstfile = "sample_ben.txt"  #  Enter the name of Output File 
+for i in range (1,70527):
+    secondfile = "article_" + str(i) + ".txt"
+    f1 = open(firstfile, 'a+')
+    f2 = open(secondfile, 'r')
+    print(i)
+    f1.write(f2.read())
+    f1.seek(0)
+    f2.seek(0)
+    f1.close()
+    f2.close()
